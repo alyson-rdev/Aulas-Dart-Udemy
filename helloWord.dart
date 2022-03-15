7d5dba037243cbe8b2c2bfe0875cda3116203c42
Alyson <alyson.rdev@gmail.com>
2022-03-15 17:14:18 -0300

    Aulas Sessão 01 a 06

---
+++ helloWord.dart
@@ -0,0 +1,38 @@
+void main() {
+  // Variaveis do tipo Strings
+
+  /*
+  String nome = "Alyson";
+  nome = "Lucas";
+  String sobrenome = "Junior";
+
+  print(nome + sobrenome);
+*/
+
+// ---  Variaveis do tipo Strings
+
+/*
+  String cor = "Azul";
+  String resultado = cor ?? "Sem cor";
+  print(resultado);
+
+*/
+
+// Variaveis do tipo inteiro / boolean
+
+/*
+  int valor = 10;
+  double valorQuebrado = 5.8;
+  print(valorQuebrado);
+  */
+
+  String resultado;
+  bool status = false;
+  //print(status);
+
+  bool isTrue = 1 < 0;
+  //print(isTrue);
+
+  resultado = status ? "Verdadeiro" : "Falso";
+  print(resultado);
+}
