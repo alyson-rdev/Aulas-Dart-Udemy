5d83dab93dbba75d124b86ad1cb2bb61c3da97e2
Alyson <alyson.rdev@gmail.com>
2022-03-16 09:09:05 -0300

    Tipos de String
    Aula do curso da udemy, tipos de string em dart

---
+++ tiposDeString.dart
@@ -0,0 +1,47 @@
+void main() {
+  // Tipos de variaveis
+
+  /*
+  String nome = "Alyson";
+  nome = "Lucas";
+  String sobrenome = "Junior";
+
+  print(nome + sobrenome);
+*/
+
+/*
+  String cor = "Azul";
+  String resultado = cor ?? "Sem cor";
+  print(resultado);
+
+*/
+
+// Variaveis do tipo inteiro / boolean /dynamic
+
+/*
+  int valor = 10;
+  double valorQuebrado = 5.8;
+  print(valorQuebrado);
+  
+
+  String resultado;
+  bool status = false;
+  //print(status);
+
+  bool isTrue = 1 < 0;
+  //print(isTrue);
+
+  resultado = status ? "Verdadeiro" : "Falso";
+  print(resultado);
+
+  */
+
+/*
+  dynamic sobrenome = "Alves";
+  dynamic valor = 50.6;
+
+  sobrenome = 100;
+  print(sobrenome);
+
+*/
+}
