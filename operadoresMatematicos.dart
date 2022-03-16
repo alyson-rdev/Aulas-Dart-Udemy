99ba78bbb322ca67a9da237a57235d79ada9dc2a
Alyson <alyson.rdev@gmail.com>
2022-03-16 11:32:59 -0300

    operadoresMatematicos
    Aula de operadores matematicos

---
+++ operadoresMatematicos.dart
@@ -0,0 +1,16 @@
+void main() {
+  double num1 = 5.5;
+  double num2 = 6;
+
+  double soma = num1 + num2;
+  print(soma);
+
+  double subtrair = num1 - num2;
+  print(subtrair);
+
+  double dividir = num1 / num2;
+  print(dividir);
+
+  double multiplicar = num1 * num2;
+  print(multiplicar);
+}
